d31c9994fc7319711490125b73e1a21bb1038994
taisedias <taisedias@gmail.com>
2015-11-23 01:18:14 -0300

    One of the player wins when at least one row has same input for all columns

--- tic-tac-toe-application/src/main/java/org/taisedias/tictactoe/Game.java
+++ tic-tac-toe-application/src/main/java/org/taisedias/tictactoe/Game.java
@@ -1,16 +1,53 @@
 package org.taisedias.tictactoe;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class Game {
-    private char[][] grid;
+    private char[][] grid = new char[3][3];
 
     public void playTurn(Player player, Position position) {
         grid[position.getRow()][position.getColumn()] = player.getInput();
     }
 
     public boolean isOver() {
-        return true;
+        return playerWins();
+    }
+
+    public boolean playerWins() {
+        return scoredRow() || scoredColumn() || scoredDiagonal();
+    }
+
+    private boolean scoredColumn() {
+        return false;
+    }
+
+    public boolean scoredDiagonal() {
+        return false;
+    }
+
+    public boolean scoredRow() {
+        boolean wins;
+
+        for (int row = 0; row < grid.length; row++) {
+            char input = grid[row][0];
+            if (!isValid(input)) {
+                continue;
+            }
+            wins = true;
+            for (int column = 1; column < grid[row].length; column++) {
+                wins = wins && (input == grid[row][column]);
+            }
+            if (wins) {
+                return wins;
+            }
+        }
+        return false;
+    }
+
+    public boolean isValid(char input) {
+        return (input == 'o') || (input == 'x');
     }
 }
--- tic-tac-toe-application/src/test/java/org/taisedias/tictactoe/GameTest.java
+++ tic-tac-toe-application/src/test/java/org/taisedias/tictactoe/GameTest.java
@@ -6,27 +6,77 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertArrayEquals;
 
 public class GameTest {
     private Player firstPlayer;
     private Player secondPlayer;
     private Game game;
+    private char o = 'o';
+    private char x = 'x';
 
     @Before
     public void setUp() {
-        firstPlayer = Player.builder().input('o').build();
-        secondPlayer = Player.builder().input('x').build();
+        firstPlayer = Player.builder().input(o).build();
+        secondPlayer = Player.builder().input(x).build();
         game = Game.builder().grid(new char[3][3]).build();
     }
 
     @Test
-    public void firstPlayerShouldWinTheGame() {
+    public void firstPlayerShouldScoreFirstRow() {
         game.playTurn(firstPlayer, Position.builder().row(0).column(0).build());
-        game.playTurn(secondPlayer, Position.builder().row(0).column(1).build());
-        game.playTurn(firstPlayer, Position.builder().row(1).column(1).build());
+        game.playTurn(secondPlayer, Position.builder().row(1).column(1).build());
+        game.playTurn(firstPlayer, Position.builder().row(0).column(1).build());
+        game.playTurn(secondPlayer, Position.builder().row(1).column(0).build());
+        game.playTurn(firstPlayer, Position.builder().row(0).column(2).build());
+        assertArrayEquals(new char[][]{{o, o, o},
+                                       {x, x, 0},
+                                       {0, 0, 0}}, game.getGrid());
+        assertThat(game.scoredRow(), is(true));
+        assertThat(game.playerWins(), is(true));
+        assertThat(game.isOver(), is(true));
+    }
+
+    @Test
+    public void secondPlayerShouldScoreSecondRow() {
+        game.playTurn(firstPlayer, Position.builder().row(0).column(0).build());
+        game.playTurn(secondPlayer, Position.builder().row(1).column(1).build());
+        game.playTurn(firstPlayer, Position.builder().row(2).column(2).build());
+        game.playTurn(secondPlayer, Position.builder().row(1).column(0).build());
+        game.playTurn(firstPlayer, Position.builder().row(0).column(2).build());
+        game.playTurn(secondPlayer, Position.builder().row(1).column(2).build());
+        assertArrayEquals(new char[][]{{o, 0, o},
+                                       {x, x, x},
+                                       {0, 0, o}}, game.getGrid());
+        assertThat(game.scoredRow(), is(true));
+        assertThat(game.playerWins(), is(true));
+        assertThat(game.isOver(), is(true));
+    }
+
+    @Test
+    public void firstPlayerShouldScoreThirdRow() {
+        game.playTurn(firstPlayer, Position.builder().row(2).column(0).build());
+        game.playTurn(secondPlayer, Position.builder().row(1).column(1).build());
+        game.playTurn(firstPlayer, Position.builder().row(2).column(1).build());
         game.playTurn(secondPlayer, Position.builder().row(1).column(0).build());
         game.playTurn(firstPlayer, Position.builder().row(2).column(2).build());
+        assertArrayEquals(new char[][]{{0, 0, 0},
+                                       {x, x, 0},
+                                       {o, o, o}}, game.getGrid());
+        assertThat(game.scoredRow(), is(true));
+        assertThat(game.playerWins(), is(true));
         assertThat(game.isOver(), is(true));
+    }
+
+    @Test
+    public void inputShouldBeValid() {
+        assertThat(game.isValid(o), is(true));
+        assertThat(game.isValid(x), is(true));
+    }
+
+    @Test
+    public void inputShouldBeInvalid() {
+        assertThat(game.isValid(game.getGrid()[2][2]), is(false));
     }
 
     @After
